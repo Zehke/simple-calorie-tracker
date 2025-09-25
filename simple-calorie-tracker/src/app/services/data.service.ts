a4d60547df57506e2fd609b4c7ed4f3774ad2152
Zehke <graphicsnerd@gmx.net>
2025-09-25 20:13:19 +0200

    [TASK] refactor dataservice

--- simple-calorie-tracker/src/app/services/data.service.ts
+++ simple-calorie-tracker/src/app/services/data.service.ts
@@ -1,83 +1,33 @@
 import { Injectable } from '@angular/core';
-
-export interface Message {
-  fromName: string;
-  subject: string;
-  date: string;
-  id: number;
-  read: boolean;
-}
+import { getPregeneratedString, Meal } from '../meal/meal.model'
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  public messages: Message[] = [
-    {
-      fromName: 'Matt Chorsey',
-      subject: 'New event: Trip to Vegas',
-      date: '9:32 AM',
-      id: 0,
-      read: false
-    },
-    {
-      fromName: 'Lauren Ruthford',
-      subject: 'Long time no chat',
-      date: '6:12 AM',
-      id: 1,
-      read: false
-    },
-    {
-      fromName: 'Jordan Firth',
-      subject: 'Report Results',
-      date: '4:55 AM',
-      id: 2,
-      read: false
-    },
-    {
-      fromName: 'Bill Thomas',
-      subject: 'The situation',
-      date: 'Yesterday',
-      id: 3,
-      read: false
-    },
-    {
-      fromName: 'Joanne Pollan',
-      subject: 'Updated invitation: Swim lessons',
-      date: 'Yesterday',
-      id: 4,
-      read: false
-    },
-    {
-      fromName: 'Andrea Cornerston',
-      subject: 'Last minute ask',
-      date: 'Yesterday',
-      id: 5,
-      read: false
-    },
-    {
-      fromName: 'Moe Chamont',
-      subject: 'Family Calendar - Version 1',
-      date: 'Last Week',
-      id: 6,
-      read: false
-    },
-    {
-      fromName: 'Kelly Richardson',
-      subject: 'Placeholder Headhots',
-      date: 'Last Week',
-      id: 7,
-      read: false
+
+  public meals: Meal[] = [];
+
+  constructor() {
+    for (let i = 0; i < 10; i++) {
+      this.meals.push(this.generateRandomMeal(i));
     }
-  ];
-
-  constructor() { }
-
-  public getMessages(): Message[] {
-    return this.messages;
   }
 
-  public getMessageById(id: number): Message {
-    return this.messages[id];
+  public getMeals(): Meal[] {
+    return this.meals;
   }
+
+  public getMealById(id: number): Meal {
+    return this.meals[id];
+  }
+
+  generateRandomMeal(mealId: number): Meal {
+    let meal: Meal = {
+        id: mealId,
+        title: getPregeneratedString(),
+        date: new Date().toLocaleDateString() + ' ' + new Date().toLocaleTimeString() 
+    };
+    return meal;
+}
 }
