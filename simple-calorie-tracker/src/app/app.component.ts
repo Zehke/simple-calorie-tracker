8067cb18510a255e5ddc82598cd08fd1ee56f8b9
Zehke <graphicsnerd@gmx.net>
2025-09-28 12:38:55 +0200

    [BonusTASK] fix app, add meta-reducer

--- simple-calorie-tracker/src/app/app.component.ts
+++ simple-calorie-tracker/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectMealCollection, selectMeals } from './state/meals.selectors';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   standalone: false,
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor( private store: Store) {}
+  meals$ = this.store.select(selectMeals);
+  mealsCollection$ = this.store.select(selectMealCollection)
+
+  ngOnInit(): void { }
 }
--- simple-calorie-tracker/src/app/app.module.ts
+++ simple-calorie-tracker/src/app/app.module.ts
@@ -1,21 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { StoreModule, ActionReducer, MetaReducer } from '@ngrx/store';
+import { localStorageSync } from 'ngrx-store-localstorage'; // <-- named import, wichtig!
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { mealsReducer } from './state/meals.reducer';
 import { mealsCollectionReducer } from './state/meals-collection.reducer';
-import { StoreModule } from '@ngrx/store';
+
+export function localStorageSyncReducer(
+  reducer: ActionReducer<any>
+): ActionReducer<any> {
+  return localStorageSync({
+    keys: ['meals', 'mealsCollection'], // Slice-Keys exakt so
+    rehydrate: true,
+  })(reducer);
+}
+
+const metaReducers: MetaReducer[] = [localStorageSyncReducer];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, 
-    IonicModule.forRoot(), 
-    AppRoutingModule, 
-    StoreModule.forRoot({meals: mealsReducer, mealsCollection: mealsCollectionReducer})],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    StoreModule.forRoot(
+      { meals: mealsReducer, mealsCollection: mealsCollectionReducer },
+      { metaReducers } 
+    ),
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
